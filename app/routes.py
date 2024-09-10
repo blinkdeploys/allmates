3110b54512988b97bfc8bf45164d801b6ee3da86
blinkdeploys <blinkdeploys@gmail.com>
2024-09-10 13:49:38 -0700

    add new pages

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify
 from flask_login import login_user, logout_user, login_required, current_user
 from .models import User, Case, CourtAppearance
 from .forms import LoginForm
@@ -13,15 +13,28 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+@main.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+
 @main.route('/')
 def index():
     context = dict(title = "AllMates",
     detail="AllMates is a case data platform that helps citizens locate and stay up-to-date in cases and court communications regarding on-going cases.",
-    links=["Find Case",
-           "Court Appearance Schedule",
-           "Locate Inmate",
-           "Visitation",
-           "Transfer Funds"
+    navigation_menu=[
+        dict(code="case", title="Cases"),
+        dict(code="schedule", title="Schedule"),
+        dict(code="locate", title="Locate"),
+        dict(code="transfer", title="Tranfers"),
+        dict(code="about", title="About"),
+    ],
+    links=[
+            dict(title="Find Case", detail="Search cases by case number and citizen detail", code="find-case", id=1),
+            dict(title="Court Appearance Schedule", detail="Get notified on your court schedule", code="court-schedule", id=2),
+            dict(title="Locate Inmate", detail="Find an inmates current location", code="locate-inmate", id=3),
+            dict(title="Visitation", detail="Check up on visitation hours", code="visitation", id=4),
+            dict(title="Transfer Funds", detail="Transfer funds to an inmate", code="funds", id=5),
            ])
 
     return render_template('index.html', context=context)
