63fcfea4f36e730c67470f1e8c920288929e448c
blinkdeploys <blinkdeploys@gmail.com>
2024-11-05 10:32:34 -0800

    secret key

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'supersecretkey'
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'Eeneetaaghaequaengoshai4jahChe4h'
     SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL') or 'postgresql://postgres:password@localhost:5432/court_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
